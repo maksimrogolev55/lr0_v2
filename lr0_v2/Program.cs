cb1252b503cd4d5e0ea502b8bc07cc876fb4a587
Maksimka <mdr10@tpu.ru>
2025-12-04 11:22:08 +0700

    Добавьте файлы проекта.

---
+++ lr0_v2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr0_v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание 2.3:
+            // Задаём переменные через консоль
+            Console.Write("x = ");
+            float x = Convert.ToSingle(Console.ReadLine());
+            Console.Write("y = ");
+            float y = Convert.ToSingle(Console.ReadLine());
+            // Задаём промежуточные переменные:
+            float x1;
+            float y1;
+            // Задаём условие:
+            if (x > y)
+            {
+                y1 = (x + y) / 2;
+                x1 = 2 * x * y;
+            }
+            else
+            {
+                x1 = (x + y) / 2;
+                y1 = 2 * x * y;
+            }
+            // Приравниваем переменные промежуточным:
+            x = x1;
+            y = y1;
+            // Выводим результат в консоль:
+            Console.WriteLine("x = " + x + "; " + "y = " + y);
+            Console.Read();
+        }
+    }
+}
